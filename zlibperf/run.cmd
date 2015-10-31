da8a16a9a3e6ffcb38fe413477aff832c74e2a8b
Mat Berchtold <mberchtold@gmail.com>
2015-10-31 18:39:59 +0100

    replaced zlib-ng patch with matbech/zlib

--- zlibperf/run.cmd
+++ zlibperf/run.cmd
@@ -14,14 +14,19 @@ echo -------
 xcopy ..\zlib-ng\x86\zlib1.dll Release\ /y >NUL
 Release\zlibperf.exe %data%
 echo.
-echo zlib-ng patch
+echo matbech/zlib
 echo -------------
-xcopy "..\zlib-ng patch\x86\zlib1.dll" Release\ /y >NUL
+xcopy "..\zlib matbech\x86\zlib1.dll" Release\ /y >NUL
 Release\zlibperf.exe %data%
 echo.
 echo zlib-ori asm
 echo ------------
 xcopy "..\zlib-ori asm\x86\zlib1.dll" Release\ /y >NUL
+Release\zlibperf.exe %data%
+echo.
+echo zlib dev
+echo --------
+xcopy "..\zlib dev\x86\zlib1.dll" Release\ /y >NUL
 Release\zlibperf.exe %data%
 echo.
 echo 64-bit
@@ -31,14 +36,19 @@ echo -------
 xcopy ..\zlib-ng\x64\zlib1.dll x64\Release\ /y >NUL
 x64\Release\zlibperf.exe %data%
 echo.
-echo zlib-ng patch
+echo matbech/zlib
 echo -------------
-xcopy "..\zlib-ng patch\x64\zlib1.dll" x64\Release\ /y >NUL
+xcopy "..\zlib matbech\x64\zlib1.dll" x64\Release\ /y >NUL
 x64\Release\zlibperf.exe %data%
 echo.
 echo zlib-ori asm
 echo ------------
 xcopy "..\zlib-ori asm\x64\zlib1.dll" x64\Release\ /y >NUL
+x64\Release\zlibperf.exe %data%
+echo.
+echo zlib dev
+echo --------
+xcopy "..\zlib dev\x64\zlib1.dll" x64\Release\ /y >NUL
 x64\Release\zlibperf.exe %data%
 
 pause
